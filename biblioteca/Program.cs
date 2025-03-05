6e6397de12be3d5ffa2a674171b0424273b4fc1f
JaiderValencia <janvame05@gmail.com>
2025-03-05 16:31:53 -0500

    agrego propiedad documento en Clientes

--- biblioteca/Program.cs
+++ biblioteca/Program.cs
@@ -120,6 +120,7 @@ public class Clientes
     public int id { get; set; }
     public string? nombre { get; set; }
     public string? apellido { get; set; }
+    public string? documento { get; set; }
     public string? direccion { get; set; }
     public string? telefono { get; set; }
     public string? correo { get; set; }
